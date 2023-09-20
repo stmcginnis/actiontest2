cb1cf401a36e3e3ebe6ca0a024c0e23a85685507
Sean McGinnis <stmcg@amazon.com>
2023-09-20 18:32:56 +0000

    Add output

--- .github/actions/list-variants/run.py
+++ .github/actions/list-variants/run.py
@@ -11,5 +11,9 @@ if __name__ == '__main__':
     if (len(sys.argv) > 2):
         token = sys.argv[2]
 
+    with open("output.env", "w") as output:
+        output.write("variants='[\"aws-dev\"]'\n")
+        output.write("aarch_enemies='[{\"variant\":\"aws-dev\",\"arch\":\"aarch64\"}]'\n")
+
 
     print('Filter-variants: %s token: %s' % (filter_variants, token != ''))
