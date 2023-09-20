0cb51b7bd13e452b3aaacacd526e328ddf197433
Sean McGinnis <stmcg@amazon.com>
2023-09-20 16:42:16 +0000

    More python

--- .github/actions/list-variants/run.py
+++ .github/actions/list-variants/run.py
@@ -1,4 +1,15 @@
 import requests
+import sys
+
+filter_variants = False
+token = ''
 
 if __name__ == '__main__':
-    print(dir(requests))
+    if (len(sys.argv) > 1):
+        filter_variants = (sys.argv[1] != 'false')
+
+    if (len(sys.argv) > 2):
+        token = sys.argv[2]
+
+
+    print('Filter-variants: %s token: %s' % (filter_variants, token != ''))
